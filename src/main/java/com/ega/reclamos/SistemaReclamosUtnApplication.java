e5225b89a74dcb643e74c7d873f49801028e7069
Eric G. Alessandrini <eric.alessandrini1@gmail.com>
2025-03-07 10:34:03 -0300

    Inicio de actualizacion del proyecto

---
+++ src/main/java/com/ega/reclamos/SistemaReclamosUtnApplication.java
@@ -0,0 +1,13 @@
+package com.ega.reclamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReclamosUtnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReclamosUtnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ega/reclamos/SistemaReclamosUtnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ega.reclamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReclamosUtnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
